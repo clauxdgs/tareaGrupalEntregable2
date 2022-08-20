4dec0eec6fe6adb52a0854e8e5745fec22275f0f
clauxdgs <108471007+clauxdgs@users.noreply.github.com>
2022-08-20 18:18:18 -0300

    start the page with the data saved in localstorage

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,12 +1,41 @@
 let arrayItems = []; //se crea un array para almacenar los items
-let cantidadItems=0;
-// se crea un contados para guardar la cantidad de items
+//let cantidadItems=0;// se crea un contados para guardar la cantidad de itemsdocument.addEventListener
+// document.addEventListener("DOMContentLoaded", function(e){
+//      document.getElementById('body').addEventListener('onload',inicio())
+// });
 
+// body.addEventListener('load', function() {
+//      console.log('La página ha terminado de cargarse!!');
+     if (localStorage.getItem('Item') != null) {
+          let htmlContentToAppend="";
+
+          arrayItems = JSON.parse(localStorage.getItem('Item'));
+          let aItems = JSON.parse(localStorage.getItem('Item'));
+          let cantidadItems = aItems.length;
+
+          for (i = 0; i < cantidadItems; i++) {
+               let elementoArray = aItems[i];
+               //para el listado creo una lista que me muestre el elemento del array
+               htmlContentToAppend += `<li class="itemLista">` + elementoArray + `</li>`;
+
+               //envio todo el contenido del 
+               document.getElementById('contenedor').innerHTML = htmlContentToAppend;
+               console.log("HOLA  "+ arrayItems)
+
+          }
+     }
+//  });
+// document.body.onload = function{
+   
+//}
 
 //creamos la funcion que escucha el boton de AGREGAR
  
  function AgregarItems(){
-   
+if(localStorage.getItem('Item')!= null){
+     arrayItems=JSON.parse(localStorage.getItem('Item'));
+}
+
      contenedorItems=document.getElementById('divcontenedor');contenedorItems.style.backgroundColor='#BEF4F9';
            /*obtenemos el valor de item que es el input 
           donde ingresamos los items 
@@ -23,29 +52,32 @@ let cantidadItems=0;
           //verificamos que el contenido del input item no sea 0 y si no es cero lo almacenamos en el arrat de items con PUSH
           if(item !== ""){ 
                
-          cantidadItems++; //empezamos al ingresar un item con 1
+          //cantidadItems++; //empezamos al ingresar un item con 1
 
-               console.log(cantidadItems); 
+               //console.log(cantidadItems); 
 
           //seteo cada item ingresado con el numero de dato que corresponde a su ingreso, del 1 para adelante y lo envio a localStorage.
-               localStorage.setItem(cantidadItems, item);
+               
           
           //Almaceno el item de acuerdo al numero de item que tendo en el arrayItems.
-               arrayItems.push(localStorage.getItem(cantidadItems));
+               arrayItems.push(item);
+               localStorage.setItem('Item', JSON.stringify(arrayItems));
           
                console.log(arrayItems);
       }
-   
-     for(i=0 ; i<arrayItems.length;i++){
-          let elementoArray = arrayItems[i];
-     //para el listado creo una lista que me muestre el elemento del array
-          htmlContentToAppend+=`<li class="itemLista">` + elementoArray + `</li>`;
+      let aItems = JSON.parse(localStorage.getItem('Item'));
+      let cantidadItems = aItems.length;
+      for (i = 0; i < cantidadItems; i++) {
+           let elementoArray = aItems[i];
+           //para el listado creo una lista que me muestre el elemento del array
+           htmlContentToAppend += `<li class="itemLista">` + elementoArray + `</li>`;
 
-     //envio todo el contenido del 
-          document.getElementById('contenedor').innerHTML = htmlContentToAppend;
-         
-     }
-  
+           //envio todo el contenido del 
+           document.getElementById('contenedor').innerHTML = htmlContentToAppend;
+
+      }
+
+ 
          
       }
 
@@ -55,11 +87,12 @@ let cantidadItems=0;
           localStorage.clear();
           //limpio el array de items y tb reseteo el conteo de items
           arrayItems = [];
-          cantidadItems = 0;
+          //cantidadItems = 0;
           //Elimino cada elemento de clase itemLista, o sea los items que envie al listado
-          document.querySelectorAll('.itemLista').forEach((element) => element.remove());;
+          document.querySelectorAll('.itemLista').forEach((el) => el.remove());;
 
      }
  
 
+ 
  
